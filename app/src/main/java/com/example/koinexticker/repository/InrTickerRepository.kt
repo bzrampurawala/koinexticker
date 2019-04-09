41af7c7cf2a57e46dbc89eb30df8a82a8530deeb
Burhan <bzrampurawala@gmail.com>
2019-04-09 18:09:27 +0530

    added repository functions

--- app/src/main/java/com/example/koinexticker/repository/InrTickerRepository.kt
+++ app/src/main/java/com/example/koinexticker/repository/InrTickerRepository.kt
@@ -1,7 +1,21 @@
 package com.example.koinexticker.repository
 
+import com.example.koinexticker.model.InrTicker
 import com.example.koinexticker.model.InrTickerDao
+import io.reactivex.Completable
+import io.reactivex.Flowable
 import javax.inject.Inject
 
 class InrTickerRepository @Inject constructor(private val inrTickerDao: InrTickerDao) {
+
+    fun insertAll(tickerData: List<InrTicker>): Completable = Completable.create{ inrTickerDao.insertAll(tickerData) }
+
+    fun insert(tickerData: InrTicker): Completable = Completable.create{ inrTickerDao.insert(tickerData) }
+
+    fun delete(tickerData: InrTicker): Completable = Completable.create{ inrTickerDao.delete(tickerData) }
+
+    fun getAll(): Flowable<List<InrTicker>> = inrTickerDao.getAll()
+
+    fun getByCoin(coin: String): Flowable<InrTicker> = inrTickerDao.getByCoin(coin)
+
 }
